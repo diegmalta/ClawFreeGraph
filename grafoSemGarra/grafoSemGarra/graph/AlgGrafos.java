7c03502efddeb342e484867041e45a1bbc4c7584
Diego Malta <diegommm@dcc.ufrj.br>
2023-02-13 19:21:01 -0300

    commit-3 Atualização de código para leitura
    Refatoração de código para melhorar a leitura

--- grafoSemGarra/grafoSemGarra/graph/AlgGrafos.java
+++ grafoSemGarra/grafoSemGarra/graph/AlgGrafos.java
@@ -7,44 +7,48 @@ public class AlgGrafos
 {
     public static void main(String[] args) {
 
-    	Scanner graph = new Scanner(System.in);
-    	new DisplayFiles().showGraphOptions();
-    	System.out.println("Select the graph you want to verify if it is a Claw-Free graph by typing the file name:");
-    	String file = graph.nextLine();
-    	
-    	Digraph g1 = new Digraph();
+    	try (Scanner graph = new Scanner(System.in)) {
+			new DisplayFiles().showGraphOptions();
+			System.out.println("Select the graph you want to verify if it is a Claw-Free graph by typing the file name:");
+			String file = graph.nextLine();
+			
+			Digraph g1 = new Digraph();
 
-        try (Scanner scanner = new Scanner(new FileReader("myfiles" + System.getProperty("file.separator")
-        	+ file))) {
+			try (Scanner scanner = new Scanner(new FileReader("myfiles" + System.getProperty("file.separator")
+				+ file))) {
 
-            String linha;
-            String[] valores;
+			    String linha;
+			    String[] valores;
 
-            while (scanner.hasNextLine()){
-                linha = scanner.nextLine();
-                valores = linha.split(" ");
+			    while (scanner.hasNextLine()){
+			        linha = scanner.nextLine();
+			        valores = linha.split(" ");
 
-                for(int i=0; i<valores.length; i++){
-                    if(!valores[i].equals("=") && i==0){
-                        g1.add_vertex(Integer.parseInt(valores[i]));
+			        for(int i=0; i<valores.length; i++){
+			            if(!valores[i].equals("=") && i==0){
+			                g1.add_vertex(Integer.parseInt(valores[i]));
 
-                    }
-                    else if(!valores[i].equals("=")){
+			            }
+			            else if(!valores[i].equals("=")){
 
-                        g1.add_vertex(Integer.parseInt(valores[i]));
-                        g1.add_edge(Integer.parseInt(valores[0]), Integer.parseInt(valores[i]));
+			                g1.add_vertex(Integer.parseInt(valores[i]));
+			                g1.add_edge(Integer.parseInt(valores[0]), Integer.parseInt(valores[i]));
 
 
-                    }
+			            }
 
-                }
+			        }
 
-            }
-            
-            g1.identifica_semGarra();
-            
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: File not found: " + e.getMessage());
-        }
+			    }
+			    
+			    g1.identifica_semGarra();
+			    
+			} catch (FileNotFoundException e) {
+			    System.out.println("Error: File not found: " + e.getMessage());
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
